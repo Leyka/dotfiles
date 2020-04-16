911bc2c7b1610437535b1ee99203372a4faae482
Skander <skander.kc@gmail.com>
2020-04-16 18:54:09 -0400

    Init scripts and add packages list

--- setup_pi.sh
+++ setup_pi.sh
@@ -1,1 +1,35 @@
 #!/bin/bash
+
+#==========
+#  Helpers
+#==========
+print() {
+  echo "==> $1"
+}
+
+green="\e[92m"
+reset="\033[0m"
+print_success() {
+  echo -e "$green==> $1$reset"
+}
+
+ask() {
+  read -p "==> $1 (y/n): " answer
+}
+
+#==========
+#  Main
+#==========
+echo "Preparing Raspberry Pi ..."
+
+# create user
+ask "Creating new user ?"
+if [[ $answer == "y" || $answer == "Y" ]]; then
+  read -p "Username: " username
+  sudo adduser $username
+  sudo usermod -a -G adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,netdev,gpio,i2c,spi ${username}
+  sudo su - $username
+fi
+
+sudo echo "hi"
+whoami
