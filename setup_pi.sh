e4d1da3d556ee0b0736cb0e57327dce10486741d
Skander <skander.kc@gmail.com>
2020-07-01 00:29:13 -0400

    Add ufw

--- setup_pi.sh
+++ setup_pi.sh
@@ -5,13 +5,17 @@ sudo timedatectl set-timezone America/Toronto
 
 # Install pkgs 
 sudo apt update -qq && sudo apt upgrade -yqq
-sudo apt install -yqq curl fish htop make mosh stow tig tmux vim
+sudo apt install -yqq curl fish htop make mosh stow tig tmux ufw vim 
 
 # Default shell: fish
 chsh -s `which fish`
 
 # Symlink dotfiles
 stow fish git tmux vim
+
+# Firewall
+sudo ufw enable
+sudo ufw allow from 192.168.1.0/24
 
 sudo apt -yqq autoremove
 sudo apt -yqq autoclean
