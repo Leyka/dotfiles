f51c2ccd9ffa54c8510e01ab854418a0156eb248
Skander <skander.kc@gmail.com>
2022-04-14 11:34:47 -0400

    Add oh my zsh to mac script

--- mac.sh
+++ mac.sh
@@ -114,7 +114,8 @@ brew cleanup
 ###############################################################################
 # ZSH
 ###############################################################################
-
+# Install oh my zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # Install oh my zsh packages
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
